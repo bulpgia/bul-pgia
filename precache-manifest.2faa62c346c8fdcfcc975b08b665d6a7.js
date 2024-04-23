1c51653e1be6de308e704909a92785a808e7b55e
bulpgia <160407976+bulpgia@users.noreply.github.com>
2024-04-23 14:43:01 +0300

    Add files via upload

---
+++ precache-manifest.2faa62c346c8fdcfcc975b08b665d6a7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cce8102dd9e0ae520633e02c6d6757ae",
+    "url": "/bul-pgia/index.html"
+  },
+  {
+    "revision": "3ef7deb3a84d69f04b19",
+    "url": "/bul-pgia/static/css/main.3cbeeea9.chunk.css"
+  },
+  {
+    "revision": "45972b624fcdcef190b5",
+    "url": "/bul-pgia/static/js/2.2b80814e.chunk.js"
+  },
+  {
+    "revision": "60f6bf9e100e456690e9ab6c9a37bfc2",
+    "url": "/bul-pgia/static/js/2.2b80814e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3ef7deb3a84d69f04b19",
+    "url": "/bul-pgia/static/js/main.51ef6fa1.chunk.js"
+  },
+  {
+    "revision": "45635c80899217d37248",
+    "url": "/bul-pgia/static/js/runtime-main.367f9554.js"
+  },
+  {
+    "revision": "c4c673ecbacb383817484666505c7437",
+    "url": "/bul-pgia/static/media/thumbsup.c4c673ec.png"
+  }
+]);
